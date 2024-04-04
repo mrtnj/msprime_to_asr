2b6e0e35319e6920a59815c2572826b514f20322
Martin Johnsson <sorill@gmail.com>
2024-04-04 21:18:39 +0200

    Fix off-by-one in processing of Macleod 2013

--- python/preprocess_macleod2013.py
+++ python/preprocess_macleod2013.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 macleod = pd.read_csv("population_histories/macleod2013_raw.csv")
 history = macleod[::-1].reset_index()
-history.loc[:,"generations_ago"] = np.cumsum(history["number_of_generations"]) - 3
+history.loc[:,"generations_ago"] = np.cumsum(history["number_of_generations"])
 
+history["generations_ago"] = history["generations_ago"].shift(1)
+history.loc[0, "generations_ago"] = 0
 
 history[["generations_ago", "Ne"]].to_csv("population_histories/macleod2013.txt", sep = "\t", index = False)
